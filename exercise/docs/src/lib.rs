30eb69ed47abf0ef361eca1caad87af2f6158855
Max <naturale@hufs.ac.kr>
2025-06-23 13:18:53 +0200

    clippy

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,6 +4,9 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+/*!A pumpkin is a cultivated winter squash in the genus Cucurbita. The term is most commonly applied to round, orange-colored squash varieties, but does not possess a scientific definition. It may be used in reference to many different squashes of varied appearance and belonging to multiple species in the Cucurbita genus.
+![pumkin image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+*/
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
@@ -15,8 +18,15 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// Big orange thing
+///
+/// # recipes
+///
+/// Recipes will be coming soon.
 pub struct Pumpkin {
+    /// The roundness of the pumpkin, as a percentage.
     pub roundness: f32,
+    /// The orangeness of the pumpkin, as a number from 8 to 27.
     pub orangeness: i32,
 }
 
@@ -24,12 +34,14 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    ///If you smash the pumpkin, it will be gone. Then it can't be used for pie. :'-(
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// 'BURNT_ORANGE` is for the orangeness field in the [Pumpkin] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
