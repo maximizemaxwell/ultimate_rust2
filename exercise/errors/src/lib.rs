efbd4191d2c83076f8840c398ed49fbae55b728a
Max <naturale@hufs.ac.kr>
2025-06-25 08:13:17 +0200

    errors

--- exercise/errors/src/lib.rs
+++ exercise/errors/src/lib.rs
@@ -16,6 +16,19 @@
 
 // pub enum DolphinError...
 
+use thiserror::Error;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum DolphinError {
+    #[error("The dolphin's too hungry")]
+    Hungry,
+    #[error("The dolphin is too young to do that")]
+    TooYoung,
+    #[error("The dolphin's name is too long and annoying to say")]
+    LongName,
+}
+
 pub struct Dolphin {
     pub name: String,
     pub age: u8,
